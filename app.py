b52ab0ec052b72f719276f9e7114ddd6f78bc365
Gabriel Valdez <lg.valdez1995@gmail.com>
2020-12-28 22:07:24 -0400

    HTML Templates Front-end getting information from back-end

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+class User():
+    def __init__(self, **data):
+        self.name = data.get("name", "")
+        self.mobile = data.get("mobile", "")
+        self.is_admin = data.get("is_admin", False)
+
 @app.route("/")
 def home():
-    return "This is the main page <h1>OH HI!</h1>"
-
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
+    gabo = User(name="Gabo",
+                mobile=60792029,
+                is_admin=True)
+    danny = User(name="Danny",
+                 mobile=7511,
+                 is_admin=False)
+    oliver = User(name="Oliver")
+    users = [gabo, danny, oliver]
+    xargs = {
+        "users": users
+    }
+    return render_template("index.html", **xargs)
 
 if __name__ == "__main__":
     app.run()
