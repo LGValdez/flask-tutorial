0847ef75a956aed768289a94330d337c213b464b
Gabriel.Valdez <gabriel.valdez@jalasoft.com>
2021-01-11 22:17:56 -0400

    Sessions - Added ignore for vscode configurations. - Profile page changes. - Base changes before and after login. - Sessions stored in the server and logout url.

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-from flask import Flask, redirect, url_for, render_template, request
+from datetime import timedelta
+from flask import Flask, redirect, url_for, render_template, request, session
 from model.user.user import USER_LIST
 from model.user.user_tools import search_user, get_userdata
 
 app = Flask(__name__)
+app.secret_key = "my_secret_key"
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+
+def get_kwargs(**values):
+    kwargs = {}
+    for key in values:
+        kwargs[key] = values[key]
+    return kwargs
 
 @app.route("/")
 def home():
-    data = {"current": "home"}
-    kwargs = {"data": data}
+    kwargs = get_kwargs(data={"current": "home"})
     return render_template("index.html", **kwargs)
 
 @app.route("/contacts")
 def contacts():
-    data = {
+    kwargs = get_kwargs(data={
         "current": "contacts",
         "contacts": USER_LIST
-    }
-    kwargs = {"data": data}
+    })
     return render_template("contacts/contacts.html", **kwargs)
 
 @app.route("/login", methods=["POST", "GET"])
@@ -27,17 +35,30 @@ def login():
         password = request.form["password"]
         user_id = search_user(username, password)
         if user_id:
-            return redirect(url_for("profile", user_id=user_id))
+            session.permanent = True
+            session["user_id"] = user_id
+            return redirect(url_for("profile"))
         else:
             data["failed"] = True
-    kwargs = {"data": data}
+    kwargs = get_kwargs(data=data)
     return render_template("login/login.html", **kwargs)
 
-@app.route("/profile/<user_id>")
-def profile(user_id):
-    data = {"user": get_userdata(user_id)}
-    kwargs = {"data": data}
-    return render_template("profile/profile.html", **kwargs)
+@app.route("/profile")
+def profile():
+    if "user_id" in session:
+        kwargs = get_kwargs(data={
+            "current": "profile",
+            "user": get_userdata(session["user_id"]),
+            })
+        return render_template("profile/profile.html", **kwargs)
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user_id", None)
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
